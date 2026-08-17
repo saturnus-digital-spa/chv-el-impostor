9ca244d319d6efbcc9a43c31bead07f8270ffda8
DoctorAivan <design.villarroel.s@gmail.com>
2026-08-17 11:30:35 -0400

    Bug fixed in production mode, asgi server

--- core/asgi.py
+++ core/asgi.py
@@ -9,18 +9,19 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import api.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             api.routing.websocket_urlpatterns
         )
     ),
 })
-
